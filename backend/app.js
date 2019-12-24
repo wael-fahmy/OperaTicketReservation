91ded8e209a3d207a13af1a05a338e543418c7de
Hosam Hazem <Hosam.Moussa98@gmail.com>
2019-12-24 17:15:29 +0300

    app.js all routs is intialized

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,17 @@ const mysql = require('mysql');
 const express = require('express');
 const Users = require('./routes/Users');
 const Auth = require('./routes/Auth');
+const Events = require('./routes/Event');
+const Halls = require('./routes/Hall');
+const Reservations = require('./routes/Reservatios');
 const app = express();
 app.use(express.json());
 
 // 
 app.use('/api/Users', Users);
-//app.use('/api/Auth', Auth);
+app.use('/api/Halls', Halls);
+app.use('/api/Events', Events);
+app.use('/api/Reservations', Reservations);
 // the port where the application run
 const port = process.env.PORT || 6000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- backend/routes/Event.js
+++ backend/routes/Event.js
@@ -3,6 +3,19 @@ const express = require('express');
 const router = express.Router();
 // creat event 
 
+router.get('/Create', async (req, res) => {
+    Id = req.body.userId;
+    console.log(Id);
+    let sql = "CAll GetUserById("+Id+")"
+    db.query(sql,(err, rows )=>
+  {
+    result = JSON.stringify(rows)
+    res.send("hello")
+    console.log (result.userId)
+  
+  }
+   )
+  
 
 
 
